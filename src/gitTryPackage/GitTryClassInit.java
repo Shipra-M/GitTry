e7dd00c851d4e642c2a028937686107054e751a2
Shipra Mamgain <shipra09_mamgain@yahoo.co.in>
2020-05-09 14:35:50 +1000

    Pushing on to git. First time!

---
+++ src/gitTryPackage/GitTryClassInit.java
@@ -0,0 +1,13 @@
+package gitTryPackage;
+import java.util.Scanner;
+public class GitTryClassInit {
+
+public static void main(String []args) {
+	System.out.print("Enter your number:");
+	Scanner input = new Scanner(System.in);
+	int number = input.nextInt();
+	input.close();
+	System.out.println("The square of your number is: "+ number*number);
+	
+}
+}
---
+++ src/gitTryPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author prana
+ *
+ */
+package gitTryPackage;
